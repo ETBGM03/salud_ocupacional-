4f7897c1f4a841501f80930dee220f4c4dceeda1
ETBGM03 <56968883+ETBGM03@users.noreply.github.com>
2020-03-27 19:14:01 -0500

    proyecto relaizado con éxito
    se terminó el proyecto con mucha intensidad, ganas y positivismo.
    es por ello, que nos damos el lujo de decir ¡está hecho!.

--- Modal.js
+++
@@ -1,8 +0,0 @@
-$('.imagen').click(function (f){
-    var sus= f.target.src;
-    var mod= '<div id="moodal" class="Modal"><img  src="'+ sus + '" class="Modal_img" ><div class="Botton_Modal" id="modal_boot">X</div></div>;'
-    $('body').append(mod);
-    $('#modal_boot').click(function(){
-      $('#moodal').remove();
-    })
-})
